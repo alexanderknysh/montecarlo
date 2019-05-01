8ca6be96e813fa4c57cda19fb33f0b35ffe52dba
Alexander Knysh <alex.knysh.u@gmail.com>
2019-05-01 13:32:49 -0400

    Added check for number of point value.

--- monte_carlo.c
+++ monte_carlo.c
@@ -7,25 +7,6 @@
 #include <time.h>
 #include "random.h"
 
-void random_seed(int rank)
-{
-  srand(time(NULL) + rank);
-}
-
-double random_real(double low, double high)
-{
-  double d;
-
-  d = (double) rand() / ((double) RAND_MAX + 1);
-  return (low + d * (high - low));
-}
-
-double
-f(double x, double y)
-{
-  return x*x+y*y;
-}
-
 int
 main(int argc, char **argv)
 {
@@ -35,15 +16,29 @@ main(int argc, char **argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   
-  random_seed(rank);
+  int n = atoi(argv[1]);
+  double n_double = atof(argv[1]);
 
-  int n = 1000;
+  if (argc != 2) {
+    mprintf("Error: number of random points is not chosen.\n");
+    exit(1);
+  }
+  else if (n < 1 || n != n_double) {
+    mprintf("Error: number of random points is not positive integer.\n");
+    exit(1);
+  }
+  else if (n % size != 0) {
+    mprintf("Error: number of random points is not divisible by number of processes.\n");
+    exit(1);
+  }
+  
+  random_seed(rank);
   
   double x0 = 0, x1 = 1, y0 = 0, y1 = 1;
   double dx = (x1 - x0) / size;
 
   double xb = x0 + dx * rank;
-  double xe = x0 + dx * (rank + 1);
+  double xe = xb + dx;
   
   mprintf("XB = %g\n", xb);
   mprintf("XE = %g\n", xe);
@@ -53,7 +48,8 @@ main(int argc, char **argv)
   double fmin = f(xmin, ymin);
   double xcur, ycur;
 
-  for (int i = 0; i < n; i++) {
+  mprintf("per-proc xmin, ymin, fmin are %g , %g, %g\n", xmin, ymin, fmin);
+  for (int i = 1; i < n; i++) {
     xcur = random_real(xb,xe);
     ycur = random_real(y0,y1);
     if (f(xcur,ycur) < f(xmin,ymin)) {
@@ -65,8 +61,7 @@ main(int argc, char **argv)
   mprintf("per-proc xmin, ymin, fmin are %g , %g, %g\n", xmin, ymin, fmin);
 
   double global_fmin;
-  MPI_Reduce(&fmin, &global_fmin, 1, MPI_DOUBLE, MPI_MIN,
-             0, MPI_COMM_WORLD);
+  MPI_Reduce(&fmin, &global_fmin, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
   
   if (rank == 0) {
     mprintf("The global minimum is %g\n", global_fmin);
